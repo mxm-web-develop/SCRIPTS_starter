0706af632f1f0fc01d1ecb7e755b416c4b169938
MxM <zhanghanfeng@mxm-mini.local>
2023-11-15 18:29:05 +0800

    merge

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ ROOT_DIR="$(dirname "$SETUP_DIR")"
 # 赋予所有脚本执行权限
 chmod +x "$SETUP_DIR/f-setup.sh" "$SETUP_DIR/b-setup.sh" "$SETUP_DIR/d-setup.sh" "$SETUP_DIR/s-setup.sh"
 
-调用前端设置脚本
+#调用前端设置脚本
 bash "$SETUP_DIR/f-setup.sh" "$ROOT_DIR"
 
 # 调用后端设置脚本
@@ -26,10 +26,10 @@ bash "$SETUP_DIR/doc-setup.sh" "$ROOT_DIR"
 
 
 # 删除已执行的脚本文件
-# rm -f "$SETUP_DIR/f-setup.sh"
-# rm -f "$SETUP_DIR/b-setup.sh"
-# rm -f "$SETUP_DIR/d-setup.sh"
-# rm -f "$SETUP_DIR/s-setup.sh"
-
+rm -f "$SETUP_DIR/f-setup.sh"
+rm -f "$SETUP_DIR/b-setup.sh"
+rm -f "$SETUP_DIR/d-setup.sh"
+rm -f "$SETUP_DIR/s-setup.sh"
+rm -f "$SETUP_DIR/doc-setup.sh"
 
 echo "所有设置已完成。"
